724f93b4db88273ce41a4062c7465af30c51a56f
Andrew Ripka <aripka@Andrews-MBP-2.roc.mn.charter.com>
2017-06-06 17:58:02 -0500

    convert to property driven to enable use of spplied environment vars

--- src/main/java/io/pivotal/sample/WebSecurityConfig.java
+++ src/main/java/io/pivotal/sample/WebSecurityConfig.java
@@ -3,11 +3,11 @@ package io.pivotal.sample;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.security.ldap.authentication.BindAuthenticator;
@@ -19,30 +19,35 @@ import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopul
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	//local ldif testing versions
-	// private String ldapURL = "ldap://localhost:8389";
-	// private String rootDn = "dc=springframework,dc=org";
-	// private String ldapUserSearchBase = "ou=people";
-	// private String ldapGroupSearchBase = "ou=groups";
-	private String ldapUserSearchFilter = "uid={0}";
-	private String ldapURL = "ldap://ldap.forumsys.com:389";
-	private String rootDn = "dc=example,dc=com";
-	private String ldapUserSearchBase = "";
-	private String ldapGroupSearchBase = "";
-	private String ldapGroupFilter = "(uniqueMember={0})";
-	private String ldapGroupCN = "SCIENTISTS";
+	@Value("${ldap.url}")
+	private String ldapURL;
+
+	@Value("${ldap.rootDN}")
+	private String rootDn;
+
+	@Value("${ldap.search.base.user}")
+	private String ldapUserSearchBase;
+
+	@Value("${ldap.search.base.group}")
+	private String ldapGroupSearchBase;
+
+	@Value("${ldap.search.filter.user}")
+	private String ldapUserSearchFilter;
+
+	@Value("${ldap.search.filter.group}")
+	private String ldapGroupFilter;
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.authenticationProvider(ldapAuthenticationProvider());
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		http.authorizeRequests().anyRequest().hasRole(ldapGroupCN).and().formLogin().and().httpBasic();
-
-	}
+	// @Override
+	// protected void configure(HttpSecurity http) throws Exception {
+	// // TODO Auto-generated method stub
+	// http.authorizeRequests().anyRequest().hasRole(ldapGroupCN).and().formLogin().and().httpBasic();
+	//
+	// }
 
 	@Bean
 	public AuthenticationProvider ldapAuthenticationProvider() throws Exception {
