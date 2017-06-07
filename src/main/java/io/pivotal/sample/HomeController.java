cd69ccbaa8d68be01b94b7179b98defe50b0b5c0
Andrew Ripka <aripka@Andrews-MBP-2.roc.mn.charter.com>
2017-06-06 21:57:02 -0500

    add thymeleaf to build landing page with logout functionality

--- src/main/java/io/pivotal/sample/HomeController.java
+++ src/main/java/io/pivotal/sample/HomeController.java
@@ -3,22 +3,28 @@ package io.pivotal.sample;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class HomeController {
-	@GetMapping("/")
-    public String index() {
-		
+	
+	
+	
+	@RequestMapping("/")
+	public String index(Model model) throws Exception {
+
 		UserDetails userDetails =
 				 (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		
 		for(GrantedAuthority auth : userDetails.getAuthorities()){
 			System.out.println(auth);
 		}
+		
 
-        return "Welcome to the Test App";
-    }
+		return "index";
+	}
+
 	
 }
